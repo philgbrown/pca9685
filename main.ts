8b62a729221a6207c480be45d60af18e07d53873
Phil Brown <76572751+philgbrown@users.noreply.github.com>
2021-01-22 12:13:43 +1100

    Update main.ts

--- main.ts
+++ main.ts
@@ -126,6 +126,7 @@ namespace limits {
         pins.i2cWriteBuffer(CHIP_ADDRESS, buf, false)       // Write to PCA9685
         basic.pause(10);                                    // Let oscillator start and settle 
         PCA9685_init = true;                                // The PCA9685 is now initialised, no need to do it again
+        basic.showString("I");
     }
 	
     /**
@@ -152,6 +153,8 @@ namespace limits {
         let buf = pins.createBuffer(2);                                     // Create a buffer for i2c bus data 
         buf[0] = REG_SERVO1_BASE + (REG_SERVO_DISTANCE * Servo - 1) + 2;    // Calculate address of LED OFF low byte register
         buf[1] = final % 256;                                               // Calculate low byte value 
+        basic.showNumber(buf[0]);
+        basic.showNumber(buf[1]);
         pins.i2cWriteBuffer(CHIP_ADDRESS, buf, false);                      // Write low byte to PCA9685 
         buf[0] = REG_SERVO1_BASE + (REG_SERVO_DISTANCE * Servo - 1) + 3;    // Calculate address of LED OFF high byte register
         buf[1] = Math.floor(final / 256);                                   // Calculate high byte value
